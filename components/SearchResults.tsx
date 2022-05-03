ab11ce869872e43a32360632072e7a27e563e6a8
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-05-03 20:49:13 -0300

    Virtualizer

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
-
+import  { List, AutoSizer, ListRowRenderer } from 'react-virtualized'
 interface SearchResultsProps {
     totalPrice: number;
     
@@ -14,15 +14,35 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({results, totalPrice, onAddToWishList}: SearchResultsProps) {
+    const rowRenderer:ListRowRenderer = ({index, key, style }) => {
+        return (
+            <div key={key} style={style}>
+            <ProductItem 
+            
+            product={product}
+            onAddToWishList={onAddToWishList} />
+
+            </div>
+           
+        )
+    }
     
     return (
         <div>
             <h2>{totalPrice}</h2>
-            {results.map(product => {
+            <List 
+            height={300}
+            rowHeight={30}
+            width={900}
+            overscanRowCount={5}
+            rowCount={results.length}
+            rowRender={}
+            />
+           {/*  {results.map(product => {
                 return (
                    <ProductItem product={product}/> 
                 )
-            })}
+            })} */}
         </div>
     )
 }
